d7f8665dd9457f77bad657db943ba8dfa26c3223
Birdy <632676124@qq.com>
2022-06-10 11:20:12 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
 var name = 'hello';
-// TODO write your code here...
 console.log(name.toUpperCase())
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
@@ -14,5 +13,4 @@ console.log(newStr)
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
-//TODO write your code here...
 console.log(money.replace(/[^0-9]/ig,""))
